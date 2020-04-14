acd5565cfa9b392360fa32398fa41322bcf2369c
James <jamesmartinez@gmail.com>
2020-04-14 08:23:48 -0500

    initial

---
+++ rest_errors/rest_error.go
@@ -0,0 +1,93 @@
+package rest_errors
+
+import (
+	//"encoding/json"
+	//"errors"
+	"fmt"
+	"net/http"
+)
+
+//RestErr a..
+type RestErr interface {
+	Message() string
+	Status() int
+	Error() string
+	Causes() []interface{}
+}
+
+type restErr struct {
+	ErrMessage string        `json:"message"`
+	ErrStatus  int           `json:"status"`
+	ErrError   string        `json:"error"`
+	ErrCauses  []interface{} `json:"causes"`
+}
+
+func (e restErr) Error() string {
+	return fmt.Sprintf("message: %s - status: %d - error: %s - causes: %v",
+		e.ErrMessage, e.ErrStatus, e.ErrError, e.ErrCauses)
+}
+
+func (e restErr) Message() string {
+	return e.ErrMessage
+}
+
+func (e restErr) Status() int {
+	return e.ErrStatus
+}
+
+func (e restErr) Causes() []interface{} {
+	return e.ErrCauses
+}
+
+// NewBadRequestError a...
+/*func NewBadRequestError(message string) *restErr{
+	return &restErr{
+		Message: message,
+		Status: http.StatusBadRequest,
+		Error: "bad_request",
+	}
+}
+
+
+func NewNotFoundError(message string) *restErr{
+	return &restErr{
+		Message: message,
+		Status: http.StatusNotFound,
+		Error: "not_fount",
+	}
+}
+
+func NewInternalServerError(message string) *RestErr{
+	return &RestErr{
+		Message: message,
+		Status: http.StatusInternalServerError,
+		Error: "Internal_server_error",
+	}
+}*/
+func NewBadRequestError(message string) RestErr {
+	return restErr{
+		ErrMessage: message,
+		ErrStatus:  http.StatusBadRequest,
+		ErrError:   "bad_request",
+	}
+}
+
+func NewNotFoundError(message string) RestErr {
+	return restErr{
+		ErrMessage: message,
+		ErrStatus:  http.StatusNotFound,
+		ErrError:   "not_found",
+	}
+}
+
+func NewInternalServerError(message string, err error) restErr {
+	result := restErr{
+		ErrMessage: message,
+		ErrStatus:  http.StatusInternalServerError,
+		ErrError:   "internal_server_error",
+	}
+	if err != nil {
+		result.ErrCauses = append(result.ErrCauses, err.Error())
+	}
+	return result
+}
